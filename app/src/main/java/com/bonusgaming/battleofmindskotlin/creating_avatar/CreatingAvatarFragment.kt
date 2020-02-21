0ccc78d4df07e5872b173cd4358b0601087928d1
bonusdev <svsvaeaeG3>
2020-02-21 13:37:53 +0300

    (Add) avatar layout file

---
+++ app/src/main/java/com/bonusgaming/battleofmindskotlin/creating_avatar/CreatingAvatarFragment.kt
@@ -0,0 +1,19 @@
+package com.bonusgaming.battleofmindskotlin.creating_avatar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bonusgaming.battleofmindskotlin.R
+
+class CreatingAvatarFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_download_assets, null)
+    }
+
+}
