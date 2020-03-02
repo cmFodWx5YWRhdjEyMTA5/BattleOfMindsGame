9f47f7dae08c8d011498841b0512204345f86637
bonusdev <devgame.devid@gmail.com>
2020-03-02 08:35:17 +0300

    (save) 02 03 2020

--- app/src/main/java/com/bonusgaming/battleofmindskotlin/main/MainFragment.kt
+++ app/src/main/java/com/bonusgaming/battleofmindskotlin/main/MainFragment.kt
@@ -6,13 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.bonusgaming.battleofmindskotlin.R
 import com.bonusgaming.battleofmindskotlin.loading_game.LoadingFragment
 import com.bonusgaming.battleofmindskotlin.tools.ActivityUtils
-import com.squareup.picasso.Picasso
 
 
 class MainFragment : Fragment() {
@@ -21,14 +20,7 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.e("123", "MainFragment created")
-        val view: View = inflater.inflate(R.layout.fragment_main, container, false)
-        Picasso.get()
-            .load("https://pixabay.com/get/5ee8d2414d50b108f5d08460962f307f173adde34e50744c7d287cdc9044c2_1280.jpg")
-            .into(view.findViewById<ImageView>(R.id.image_view_logo_main))
-
-
-
-        return view
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,10 +28,10 @@ class MainFragment : Fragment() {
     }
 
     fun initViews(view: View) {
-        val startButton: Button = view.findViewById(R.id.button_start)
+        val startButton: CardView = view.findViewById(R.id.button_start)
         startButton.setOnClickListener { menuViewModel.onStartClick() }
 
-        menuViewModel.getLiveFragmentState().observe(this, Observer {
+        menuViewModel.getLiveFragmentState().observe(viewLifecycleOwner, Observer {
             ActivityUtils.replaceFragment(activity!!.supportFragmentManager, LoadingFragment())
         })
 
