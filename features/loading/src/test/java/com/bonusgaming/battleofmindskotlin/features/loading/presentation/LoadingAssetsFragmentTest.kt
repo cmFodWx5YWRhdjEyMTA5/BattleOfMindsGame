1cdf368aeb8a9214c269948638ac795aecbfed4e
bonusdev <devgame.devid@gmail.com>
2020-03-30 22:49:57 +0300

    (save) save 30 03 2020

--- features/loading/src/test/java/com/bonusgaming/battleofmindskotlin/features/loading/presentation/LoadingAssetsFragmentTest.kt
+++ features/loading/src/test/java/com/bonusgaming/battleofmindskotlin/features/loading/presentation/LoadingAssetsFragmentTest.kt
@@ -1,19 +1,20 @@
 package com.bonusgaming.battleofmindskotlin.features.loading.presentation
 
 import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import com.bonusgaming.battleofmindskotlin.features.loading.R
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import androidx.test.core.app.ActivityScenario.launch
-import com.bonusgaming.battleofmindskotlin.base_ui.presentation.MainActivityView
 
 @RunWith(RobolectricTestRunner::class)
 class LoadingAssetsFragmentTest {
 
     @Test
-    fun `should show download text`(){
+    fun `should show download text`() {
         val fragmentArgs = Bundle()
 
-        val scenario = launch(MainActivityView::class.java)
+//        val scenario = launch(MainActivityView::class.java)
+        val scenario = launchFragmentInContainer<LoadingAssetsFragment>(Bundle.EMPTY, R.style.AppTheme)
     }
 }
